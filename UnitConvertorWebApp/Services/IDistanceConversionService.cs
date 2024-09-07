644caf7cd0f0c443be29a79d6b2f102da8fa66f5
Frederik <frederik.m.hansen@gmail.com>
2024-09-07 23:21:08 +0200

    made some more distance conversions and also changed the ui a little

--- UnitConvertorWebApp/Services/IDistanceConversionService.cs
+++ UnitConvertorWebApp/Services/IDistanceConversionService.cs
@@ -2,12 +2,31 @@
 {
     public interface IDistanceConversionService
     {
+        // Metric to Metric
+        double MillimetersToCentimeters(double millimeters);
+        double CentimetersToMillimeters(double centimeters);
+        double CentimetersToMeters(double centimeters);
+        double MetersToCentimeters(double meters);
+        double MetersToKilometers(double meters);
+        double KilometersToMeters(double kilometers);
+
+        // Metric to Imperial
         double MetersToFeet(double meters);
-
-        double FeetToMeters(double feet);
-
+        double MetersToYards(double meters);
         double KilometersToMiles(double kilometers);
+        double KilometersToFeet(double kilometers);
 
+        // Imperial to Metric
+        double InchesToCentimeters(double inches);
+        double FeetToMeters(double feet);
+        double YardsToMeters(double yards);
         double MilesToKilometers(double miles);
+        double FeetToKilometers(double feet);
+
+        // Imperial to Imperial
+        double FeetToYards(double feet);
+        double YardsToFeet(double yards);
+        double InchesToFeet(double inches);
+        double FeetToInches(double feet);
     }
 }
--- UnitConvertorWebApp/Services/Implementations/DistanceConversionService.cs
+++ UnitConvertorWebApp/Services/Implementations/DistanceConversionService.cs
@@ -2,25 +2,27 @@
 {
     public class DistanceConversionService : IDistanceConversionService
     {
-        public double MetersToFeet(double meters)
-        {
-            return meters * 3.28084;
-        }
+        public double MillimetersToCentimeters(double millimeters) => millimeters / 10.0;
+        public double CentimetersToMillimeters(double centimeters) => centimeters * 10.0;
+        public double CentimetersToMeters(double centimeters) => centimeters / 100.0;
+        public double MetersToCentimeters(double meters) => meters * 100.0;
+        public double MetersToKilometers(double meters) => meters / 1000.0;
+        public double KilometersToMeters(double kilometers) => kilometers * 1000.0;
 
-        public double FeetToMeters(double feet)
-        {
-            return feet / 3.28084;
-        }
+        public double MetersToFeet(double meters) => meters * 3.28084;
+        public double MetersToYards(double meters) => meters * 1.09361;
+        public double KilometersToMiles(double kilometers) => kilometers * 0.621371;
+        public double KilometersToFeet(double kilometers) => kilometers * 3280.84;
 
-        public double KilometersToMiles(double kilometers)
-        {
-            return kilometers * 0.621371;
-        }
+        public double InchesToCentimeters(double inches) => inches * 2.54;
+        public double FeetToMeters(double feet) => feet / 3.28084;
+        public double YardsToMeters(double yards) => yards * 0.9144;
+        public double MilesToKilometers(double miles) => miles / 0.621371;
+        public double FeetToKilometers(double feet) => feet * 0.0003048;
 
-        public double MilesToKilometers(double miles)
-        {
-            return miles / 0.621371;
-        }
-
+        public double FeetToYards(double feet) => feet / 3.0;
+        public double YardsToFeet(double yards) => yards * 3.0;
+        public double InchesToFeet(double inches) => inches / 12.0;
+        public double FeetToInches(double feet) => feet * 12.0;
     }
 }

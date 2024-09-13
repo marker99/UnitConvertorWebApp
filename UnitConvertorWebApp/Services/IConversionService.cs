d24788119105450e33e9deedcdc2c128e2385bba
Frederik Marker Hansen <frederik.m.hansen@gmail.com>
2024-09-13 22:41:57 +0200

    dynamic page with all conversions done. Now trying to make a more compact page

--- UnitConvertorWebApp/Services/IConversionService.cs
+++ UnitConvertorWebApp/Services/IConversionService.cs
@@ -4,6 +4,16 @@ namespace UnitConvertorWebApp.Services
 {
     public interface IConversionService
     {
+        /// <summary>
+        /// Should be able to handle every type of conversions
+        /// </summary>
+        /// <param name="quantityName"></param>
+        /// <param name="value"></param>
+        /// <param name="fromUnit"></param>
+        /// <param name="toUnit"></param>
+        /// <returns></returns>
+        ConversionResult Convert(string quantityName, double value, string fromUnit, string toUnit);
+
         public ConversionResult ConvertDistance(double value, string fromUnit, string toUnit);
         public ConversionResult ConvertWeight(double value, string fromUnit, string toUnit);
         public ConversionResult ConvertTemperature(double value, string fromUnit, string toUnit);
--- UnitConvertorWebApp/Services/Implementations/ConversionService.cs
+++ UnitConvertorWebApp/Services/Implementations/ConversionService.cs
@@ -6,6 +6,25 @@ namespace UnitConvertorWebApp.Services.Implementations
 {
     public class ConversionService : IConversionService
     {
+        public ConversionResult Convert(string quantityName, double value, string fromUnit, string toUnit)
+        {
+            // Get the QuantityInfo for the given quantity name
+            var quantityInfo = Quantity.ByName[quantityName];
+
+            // Parse the units based on the quantity
+            var fromUnitEnum = (Enum)Enum.Parse(quantityInfo.UnitType, fromUnit);
+            var toUnitEnum = (Enum)Enum.Parse(quantityInfo.UnitType, toUnit);
+
+            // Convert the value using the UnitConverter
+            var convertedValue = UnitConverter.Convert(value, fromUnitEnum, toUnitEnum);
+
+            // Get the abbreviation for the target unit
+            var abbreviation = UnitsNetSetup.Default.UnitAbbreviations.GetDefaultAbbreviation(toUnitEnum);
+
+            // Return the result with the converted value and abbreviation
+            return new ConversionResult(convertedValue, abbreviation);
+        }
+
         public ConversionResult ConvertDistance(double value, string fromUnit, string toUnit)
         {
             var fromUnitEnum = Enum.Parse<LengthUnit>(fromUnit, true);
